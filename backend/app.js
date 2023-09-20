fb0ed2beaa7f897e1601d38b94bd15c2c13c6bec
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2023-09-20 12:22:00 -0300

    first commit, login backend

---
+++ backend/app.js
@@ -0,0 +1,135 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+const posts = [
+  {
+    name: 'Valeria',
+    title: 'Ushuaia',
+  },
+  {
+    name: 'Javier',
+    title: 'Italia',
+  },
+];
+
+let refreshTokens = [];
+
+//Get users
+app.get('/api/users', authenticateToken, (req, res) => {
+  res.json(users);
+});
+
+//Get posts (example)
+app.get('/api/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter((el) => el.name === req.user.name));
+});
+
+//Register users
+app.post('/api/users/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10); //las contraseñas siempre se deben pasar como string para que funcionen!!
+
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).send('User registered');
+  } catch (error) {
+    res.status(500).send('error');
+  }
+});
+
+//Users login
+app.post('/api/users/login', async (req, res) => {
+  const user = users.find((el) => el.name === req.body.name);
+  console.log(user);
+  if (!user) {
+    return res.status(400).send('Cannot find user');
+  }
+  try {
+    //Authentication
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      // res.send('Login successfull');
+
+      //Authorization
+      const username = req.body.name;
+      const user = { name: username };
+      const accessToken = generateAccessToken(user);
+      const refreshToken = generateRefreshToken(user);
+      refreshTokens.push(refreshToken);
+      res.json({ accessToken: accessToken, refreshToken: refreshToken });
+    } else {
+      res.send('Not allowed');
+    }
+  } catch (error) {
+    res.status(500).send('error');
+  }
+});
+
+//Refresh token after login (access token)
+app.post('/api/users/token', (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    const refreshToken = generateRefreshToken({ name: user.name });
+      refreshTokens.push(refreshToken);
+    res.json({ accessToken: accessToken, refreshToken:refreshToken });
+  });
+});
+
+//Logout users
+app.delete('/api/users/logout', (req, res) => {
+  refreshTokens = refreshTokens.filter((el) => el !== req.body.token);
+  res.sendStatus(204);
+});
+
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '40s' });
+}
+
+function generateRefreshToken(user) {
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '2h' });
+}
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    res.sendStatus(401);
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+app.listen(8081, () => {
+  console.log('listening on port 8081');
+});
+
+//login común con useState login setlogin (ves pantalla gral o pantalla de quien ya se logueó)
+//guarda access token  y refresh token en local storage
+//manda en cada request el access token en el header. Headers: Bearer + accestoken
+//hace un useeffect en app que tiene un setinterval que escribe al endpoint token cada x minutos y le pasa el refresh token y actuliza los valores de access y refresn en el localstorage
+//este endpoint debe traer un nuevo refresh además el access.
+
+//next como si fuera react. Typescript. css puro
+//poner api/ a estas rutas
+//crear carpetas y capas
+//conexión a moongose y poner en el word (verla de EPI)
+//env terminarlo
+
+//próx proyecto si tiene solo bd con login usar next con su server (video de fazt)
+
