bc9aa4b3cc65c0b3b151fd28be7247f982d58b0b
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2023-12-14 08:26:00 -0300

    utils

--- frontend/epicalls/src/redux/utils.js
+++ frontend/epicalls/src/redux/utils.js
@@ -1,3 +1,5 @@
 //export const baseURL = 'http://localhost:8081/'
 
-export const baseURL = 'https://epi-calls-3rzwkg8hd-valeriapaulinalustres.vercel.app/'
+//export const baseURL = 'https://epi-calls-3rzwkg8hd-valeriapaulinalustres.vercel.app/'
+
+export const baseURL ='https://epi-calls.vercel.app/'
